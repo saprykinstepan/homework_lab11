7590c8a304fbd7f2d9c84804ea91b2c80509d889
Stepan <saprykinstepa442@gmail.com>
2025-11-04 10:23:41 +0300

    Добавьте файлы проекта.

---
+++ homework.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define MAX_SIZE 100
+
+int main() {
+    setlocale(LC_ALL, "");
+    int n, count = 0;
+    printf("Введите количество элементов в массиве: ");
+    scanf("%d", &n);
+
+    if (n > MAX_SIZE) {
+        printf("Ошибка: максимальный размер массива - %d\n", MAX_SIZE);
+        return 1;
+    }
+
+    int arr[MAX_SIZE];
+
+    printf("Введите %d элементов массива:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 10 && i % 2 == 0) {
+            count++;
+        }
+    }
+    printf("Количество элементов больших 10, которые находятся на чётных позициях: %d\n", count);
+	return 0;
+}
